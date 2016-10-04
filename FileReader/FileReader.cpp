97c0bb804f382eadfb626f280a0a7742442e5bd4
dcat52 <Davis.Catherman@pcs.cnu.edu>
2016-10-04 11:25:32 -0400

    Base code provided by Prof. Perkins

---
+++ FileReader/FileReader.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include "..\Include\FileReader.h"
+#include "..\Include\constants.h"
+#include "..\Include\Debug_Help.h"
+using namespace std;
+using namespace KP_FileReaderClass;
+
+//TODO Fill this in
