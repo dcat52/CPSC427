634133da5f9b27e7dd92dec2eba84e55a9ff25c6
Davis <Davis@DESKTOP-5T8NBS7>
2016-10-19 20:35:41 -0400

    Current code with basic methods StringParserClass.cpp

--- StringParser/StringParserClass.cpp
+++ StringParser/StringParserClass.cpp
@@ -8,4 +8,47 @@
 using namespace std;
 using namespace KP_StringParserClass;
 
-//TODO Fill this in
+StringParserClass::StringParserClass(void)
+{
+	setTags(START_TAG, END_TAG);
+	areTagsSet = false;
+	lastError = UNINITIALIZED;
+}
+
+StringParserClass::~StringParserClass(void)
+{
+	//be safe
+	cleanup();
+}
+
+int StringParserClass::getLastError()
+{
+	return lastError;
+}
+
+bool StringParserClass::setTags(const char * pStartTag, const char * pEndTag)
+{
+	this->pStartTag = (char*)pStartTag;
+	this->pEndTag = (char*)pEndTag;
+	return true;
+}
+
+bool StringParserClass::getDataBetweenTags(char * pDataToSearchThru, vector<string>& myVector)
+{
+	for (int i = 0; i < strlen(pDataToSearchThru); i++)
+	{
+		//findTag(this->pStartTag, pDataToSearchThru, pDataToSearchThru);
+		//if (pDataToSearchThru[i] == 
+	}
+
+	return false;
+}
+
+void StringParserClass::cleanup()
+{
+}
+
+bool StringParserClass::findTag(char * pTagToLookFor, char *& pStart, char *& pEnd)
+{
+	return false;
+}
