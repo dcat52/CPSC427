5dbd969e3965263eaa19cea09fb3f30413769bdc
Kimberly O Jimenez <kimberly.jimenez.14@cnu.edu>
2016-10-20 22:42:47 -0400

    Started working on FileReader.cpp by: -opening input file -loop if initial file is not found -check if user enters X to exit

--- FileReader/FileReader.cpp
+++ FileReader/FileReader.cpp
@@ -7,3 +7,37 @@ using namespace std;
 using namespace KP_FileReaderClass;
 
 //TODO Fill this in
+
+FileReader::FileReader()
+{
+}
+
+FileReader::~FileReader()
+{
+}
+
+int FileReader::getFileContents(const std::string filename, std::string & contents)
+{
+	ifstream myFile;
+	//opens file
+	myFile.open(filename);					
+
+	//if file does not open then...
+	while (!myFile.is_open()) {
+		cout << "Could not find file, please enter a filename or an X to exit: ";
+
+		if (filename == EXITCHAR) {
+			return USER_CHOSE_TO_EXIT;
+		}
+
+		myFile.open(filename);
+	}
+
+	myFile.close();
+	return 0;
+}
+
+int FileReader::ReadTheWholeFile(const std::string & filename)
+{
+	return 0;
+}
